fe6ded9d83ae7d71031bf875d614757cec7be528
ffati <alnaserfatimah344@gmail.com>
2023-05-06 17:08:54 +0300

    DataBase has been created

---
+++ app/src/main/java/com/example/bookshelf/DBhelper.java
@@ -0,0 +1,26 @@
+package com.example.bookshelf;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBhelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "BOOKSHELF.db";
+
+
+    public DBhelper(@Nullable Context context) {
+        super(context, DBNAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+}
