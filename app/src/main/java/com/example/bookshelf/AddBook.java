9426c04c26517976cd63c3a1bb39e434f3c6c0a7
Alin <AlinAltowim@gmail.com>
2023-05-08 18:03:43 +0300

    add interface

---
+++ app/src/main/java/com/example/bookshelf/AddBook.java
@@ -0,0 +1,16 @@
+package com.example.bookshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+
+    }
+}
