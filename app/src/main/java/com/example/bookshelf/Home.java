a549b6c71d321fcc1ac132d99f96a23930a17b01
Ashwaq361 <ash23wag@gmail.com>
2023-05-11 14:05:35 +0300

    Add log out button

--- app/src/main/java/com/example/bookshelf/Home.java
+++ app/src/main/java/com/example/bookshelf/Home.java
@@ -2,13 +2,25 @@ package com.example.bookshelf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
-
+private Button LogOutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        LogOutButton=findViewById(R.id.LogOutButton);
+        LogOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,main.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
